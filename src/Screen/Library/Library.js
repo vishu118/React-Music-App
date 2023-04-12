394e435982a3c0728a395d055b93e1043afd028c
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-12 14:00:40 +0530

    fetceing playlist

--- src/Screen/Library/Library.js
+++ src/Screen/Library/Library.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import APIKit from "../../Spotify"
 
 const Library = () => {
+const [playlist , setPlaylist] = useState(null)
+
+useEffect(()=>{
+  APIKit.get("me/playlists").then((res)=>{
+    setPlaylist(res.data.items)
+  })
+}, [])
+
   return (
-    <div className='screen-container'>library</div>
+    <div className='screen-container'>
+      {
+        playlist?.map((playlist)=>
+          <div>{playlist.name}</div>
+        )
+      }
+    </div>
   )
 }
 
