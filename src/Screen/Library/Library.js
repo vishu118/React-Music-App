805b74da2ebdd85f719b3e512bf36a0fed8a61e1
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-12 14:24:02 +0530

    playlist and images fetch

--- src/Screen/Library/Library.js
+++ src/Screen/Library/Library.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import APIKit from "../../Spotify"
+import "./Library.css"
 
 const Library = () => {
 const [playlist , setPlaylist] = useState(null)
@@ -7,16 +8,30 @@ const [playlist , setPlaylist] = useState(null)
 useEffect(()=>{
   APIKit.get("me/playlists").then((res)=>{
     setPlaylist(res.data.items)
+    console.log(res.data.items.images[0].url)
   })
 }, [])
 
   return (
-    <div className='screen-container'>
-      {
-        playlist?.map((playlist)=>
-          <div>{playlist.name}</div>
-        )
-      }
+    <div className="screen-container">
+      <div className="library-body">
+        {playlist?.map((playlist) => (
+          <div
+            className="playlist-card"
+            key={playlist.id}
+  
+          >
+            <img
+              src={playlist.images[0].url}
+              className="playlist-image"
+              alt="Playlist-Art"
+            />
+            <p className="playlist-title">{playlist.name}</p>
+            <p className="playlist-subtitle">{playlist.tracks.total} Songs</p>
+           
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
