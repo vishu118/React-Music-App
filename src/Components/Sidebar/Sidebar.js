25a439f12863113b25c98a2e62abde59478aa3d1
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-12 12:32:37 +0530

    fetching data form user profile

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import './Sidebar.css'
-import SidebarButtons from './SidebarButtons'
-import {MdFavorite} from 'react-icons/md'
-import {FaGripfire, FaPlay, FaSignOutAlt} from 'react-icons/fa'
-import {IoLibrary} from 'react-icons/io5'
-import {MdSpaceDashboard} from 'react-icons/md'
+import React, { useEffect, useState } from "react";
+import "./Sidebar.css";
+import SidebarButtons from "./SidebarButtons";
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay, FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
+import apiClient from "../../Spotify";
 
 const Sidebar = () => {
+  const [userImage, setUserImage] = useState(
+    "https://www.shutterstock.com/image-vector/man-icon-vector-260nw-1040084344.jpg"
+  );
+  useEffect(() => {
+    apiClient.get('me').then(response => setUserImage(response.data.images[0].url))
+  }, []);
   return (
-    <div className='sidebar-container'>
-      <img src='https://www.shutterstock.com/image-vector/man-icon-vector-260nw-1040084344.jpg' className='profile-image' alt='profile'/>
+    <div className="sidebar-container">
+      <img src={userImage} className="profile-image" alt="profile" />
       <div className="sidebarButton">
-        <SidebarButtons title = "feed"  to = "/Feed" icon = {<MdSpaceDashboard/>} />
-        <SidebarButtons title = "trending"  to = "/Trending" icon = {<FaGripfire/>}/>
-        <SidebarButtons title = "player"  to = "/Player" icon = {<FaPlay/>}/>
-        <SidebarButtons title = "favorites"  to = "/Favorites" icon = {<MdFavorite/>}/>
-        <SidebarButtons title = "library"  to = "/" icon = {<IoLibrary/>}/>
+        <SidebarButtons title="feed" to="/Feed" icon={<MdSpaceDashboard />} />
+        <SidebarButtons title="trending" to="/Trending" icon={<FaGripfire />} />
+        <SidebarButtons title="player" to="/Player" icon={<FaPlay />} />
+        <SidebarButtons
+          title="favorites"
+          to="/Favorites"
+          icon={<MdFavorite />}
+        />
+        <SidebarButtons title="library" to="/" icon={<IoLibrary />} />
       </div>
-      <SidebarButtons title = "Sign Out"  to = "" icon = {<FaSignOutAlt/>}/>
+      <SidebarButtons title="Sign Out" to="" icon={<FaSignOutAlt />} />
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
