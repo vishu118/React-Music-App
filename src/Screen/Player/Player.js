f10dac49b382f7634e68382d8d89d2de5f26757a
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-13 17:44:33 +0530

    .

--- src/Screen/Player/Player.js
+++ src/Screen/Player/Player.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "./Players.css"
+import { useLocation } from 'react-router-dom'
+
 
 const Player = () => {
+const location = useLocation();
+console.log(location)
+
+useEffect(()=>{
+if(location.state){
+apiClient.get('play')
+}
+}, [])
   return (
-    <div className='screen-container'>Player</div>
+    <div className='screen-container'>
+      <div className="left-playerbody"></div>
+      <div className="right-playerbody"></div>
+    </div>
   )
 }
 
