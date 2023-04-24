12ad1735cdf5785a71d7d28ff0fb64217db0b1bf
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-24 12:21:01 +0530

    .

--- src/Screen/Player/Player.js
+++ src/Screen/Player/Player.js
@@ -1,21 +1,36 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Players.css"
 import { useLocation } from 'react-router-dom'
+import apiClient from "../../Spotify"
 
 
 const Player = () => {
 const location = useLocation();
-console.log(location)
+const [ tracks,setTracks ] =useState('')
+const [ currentTracks,setcurrentTracks ] =useState({})
+const [ currentIndex,setcurrentIndex ] =useState(0);
+
+
 
 useEffect(()=>{
 if(location.state){
-apiClient.get('play')
+apiClient.get("playlists/" + location.state?.id+"/tracks")
+.then(res=>{
+  setTracks(res.data.items)
+  setcurrentTracks(res.data.items[0].track)
 }
-}, [])
+  )
+}
+}, [location.state])
   return (
     <div className='screen-container'>
-      <div className="left-playerbody"></div>
-      <div className="right-playerbody"></div>
+      <div className="left-playerbody">
+
+      </div>
+      <div className="right-playerbody">
+        <SongCard/>
+        <Queue/>
+      </div>
     </div>
   )
 }
