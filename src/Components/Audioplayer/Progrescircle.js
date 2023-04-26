2f7ac926cc00c50fa714e1b21a27bbf6b1e4f199
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-26 10:45:46 +0530

    .

--- src/Components/Audioplayer/Progrescircle.js
+++ src/Components/Audioplayer/Progrescircle.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Progresscircle.css"
 const Circle = ({color,percentage,size,strokeWidth})=>{
   const radius = size/2 - 10;
-  const circ = ((2*Math.PI * r) - 20)
+  const circ = ((2*Math.PI * radius) - 20)
   const strokePct = ((100 - Math.round(percentage) * circ))/100;
   return (
     <Circle 
@@ -22,7 +22,28 @@ const Circle = ({color,percentage,size,strokeWidth})=>{
 const Progrescircle = ({percentage,isPlaying,size,color,image}) => {
 
   return (
-    <div>
+    <div className="progress-circle flex">
+    <svg width={size} height={size}>
+    <g>
+      <Circle strokeWidth={"0.4rem"} color="#3B4F73" size={size}  />
+      <Circle
+            strokeWidth={"0.6rem"}
+            color={color}
+            percentage={percentage}
+            size={size}
+          />
+    </g>
+
+    <defs>
+          <clipPath id="myCircle">
+            <circle cx="50%" cy="50%" r={size / 2 - 30} fill="#FFFFFF" />
+          </clipPath>
+          <clipPath id="myInnerCircle">
+            <circle cx="50%" cy="50%" r={size / 2 - 100} fill="#FFFFFF" />
+          </clipPath>
+        </defs>
+
+    </svg>
 
     </div>
   )
